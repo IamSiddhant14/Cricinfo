62176545a8a3c5163c3f9fe0758965c18d6fb237
Siddhant <siddhantsharma669@gmail.com>
2022-01-30 04:26:21 +0530

    extracted reuired info from the website

---
+++ matchInfo.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/ball-by-ball-commentary',cb);
+
+function cb(error , response , html){
+    if( error ){
+        console.error('error' , error);
+    }else{
+        handleHtml(html);
+    }
+
+}
+
+function handleHtml(html){
+    
+    let selTool = cheerio.load(html);
+    let contentArr = selTool('.match-comment-wrapper .match-comment-long-text');
+
+    let ans = selTool(contentArr[0]).text();
+    console.log(`
+    
+    
+    
+    `,ans);
+    console.log(`
+    
+    
+    
+    `);
+
+}
